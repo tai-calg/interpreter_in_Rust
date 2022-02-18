77d449f90d5c63188b03dceb7d0fea1da8e0c1de
taniguchi <taniguchi123456@icloud.com>
2022-02-18 22:38:20 +0900

    first day

---
+++ target/debug/deps/interpreter_rust-f574b72d6be60383.d
@@ -0,0 +1,5 @@
+/Users/taniguchitaichi/development/rustDev/interpreter-rust/target/debug/deps/interpreter_rust-f574b72d6be60383.rmeta: src/main.rs
+
+/Users/taniguchitaichi/development/rustDev/interpreter-rust/target/debug/deps/interpreter_rust-f574b72d6be60383.d: src/main.rs
+
+src/main.rs:
